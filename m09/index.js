3d9fd2c197bcc35ac47f7495cc07551bd7229ed4
Sophia Franks <129785396+sophiafranks@users.noreply.github.com>
2023-06-02 23:43:52 -0700

    Update index.js

--- m09/index.js
+++ m09/index.js
@@ -1,20 +1,19 @@
-var  mybutton = document.querySelector("clearbutton");
-mybutton.addEventListener("click", function(event) {
+var clearbutton = document.getElementById("clearbutton");
+clearbutton.addEventListener("click", function(event) {
+  var element = document.getElementsByClassName("dot");
+  while (element.length > 0) {
+    element[0].parentNode.removeChild(element[0]);
+  }
 
+  // Let's stop the propagation of events
+  
+  event.stopPropagation();
+});
 
-var element = document.getElementsByTagName("div");
-for (index = element.length - 1; index >= 0; index--) {
-    element[index].parentNode.removeChild(element[index]);
-}
-
-// Let us stop the propagation of events
-
-event.stopPropagation();
-  });
-  addEventListener("click", function(event) {
-    var dot = document.createElement("div");
-    dot.className = "dot";
-    dot.style.left = (event.pageX - 4) + "px";
-    dot.style.top = (event.pageY - 4) + "px";
-    document.body.appendChild(dot);
-  });
+document.addEventListener("click", function(event) {
+  var dot = document.createElement("div");
+  dot.className = "dot";
+  dot.style.left = (event.pageX - 4) + "px";
+  dot.style.top = (event.pageY - 4) + "px";
+  document.body.appendChild(dot);
+});
